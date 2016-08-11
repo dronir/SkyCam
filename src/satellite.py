080bc7548371814ce8aaa46cb6304846169db724
Olli Wilkman <olli.wilkman@iki.fi>
2016-08-11 15:32:47 +0300

    Remove needless stuff from satellites

--- src/satellite.py
+++ src/satellite.py
@@ -16,11 +16,6 @@ def get_Hovi():
 
 class SatelliteHandler:
     def __init__(self, ax, config, obs=None):
-        """Initialize satellite handler with names.
-        
-        'names' is a list of the names of the desired satellites, 'filename' gives the
-        file containing the orbit information in Two-Line Element format."""
-        
         self.DEBUG = config["main"]["debug_level"]
         if self.DEBUG >= 1:
             print("SatelliteHandler: Initializing.")
@@ -57,54 +52,6 @@ class SatelliteHandler:
         if self.DEBUG >= 2:
             sats = ", ".join(sorted(self.satellites.keys()))
             print("SatelliteHandler: following these satellites: {}".format(sats))
-        
-    def trace(self, idx, date=None, interval=1, N=7, offset=-1):
-        """Returns the sky path of the ith satellite in the handler.
-        
-        Date defaults to now, interval is in minutes, N is the number of points and
-        offset is a shift in time. The defaults produce time points from -2 to +14
-        minutes from now."""
-        
-        if data is None:
-            date = datetime.datetime.now()
-        date = date - datetime.timedelta(seconds=date.second)
-        loc = []
-        sat = self.satellites[idx]
-        for i in range(N):
-            self.observer.date = date + datetime.timedelta(minutes=interval*(i+offset))
-            t = self.observer.date.tuple()
-            timestamp = "{:d}:{:0d}".format(t[3], t[4])
-            sat.compute(self.observer)
-            loc.append((timestamp, float(sat.alt), float(sat.az)))
-        return loc
-        
-    def loc(self, idx, date=None):
-        """Get the position of the ith satellite at a given time."""
-        if date is None:
-            date = datetime.datetime.now()
-        self.observer.date = date
-        sat = self.satellites[idx]
-        sat.compute(self.observer)
-        return (float(sat.alt), float(sat.az))
-        
-    def add_satellite(self, name):
-        """Add a new satellite to the list."""
-        self.names.append(name)
-        self.update()
-    
-    def list_visible(self, date=None):
-        """Return a list with the names and positions of all visible satellites."""
-        if date is None:
-            date = datetime.datetime.now()
-        self.observer.date = date
-        output = []
-        for sat, point in self.satellites.values():
-            sat.compute(self.observer)
-            alt = float(sat.alt)
-            az = float(sat.az)
-            if alt > 0:
-                output.append((sat.name, alt, az))
-        return output
     
     def draw(self):
         if self.DEBUG >= 3:
@@ -118,9 +65,5 @@ class SatelliteHandler:
                 point.set_data([az], [90 - alt*180/pi])
             else:
                 point.set_data([], [])
-    
-if __name__=="__main__":
-    H = SatelliteHandler("geodetic.txt")
-    print(H.list_visible())
 
 
