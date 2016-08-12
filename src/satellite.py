3d23d2d5f0610b669a2d23d0c9e51a10e4d5c50f
Olli Wilkman <olli.wilkman@iki.fi>
2016-08-12 10:45:32 +0300

    Satellite names

--- src/satellite.py
+++ src/satellite.py
@@ -6,13 +6,12 @@ from numpy import pi
 # The SatelliteHandler maintains a list of satellites and their locations, and draws them
 # on the given Axes object when requested.
 
-def get_Hovi():
-    """Returns a PyEphem observer for Metsähovi."""
-    Hovi = ephem.Observer()
-    Hovi.lat = 60.217165
-    Hovi.lon = 24.394562
-    Hovi.elevation = 80
-    return Hovi
+
+Hovi = ephem.Observer()
+Hovi.lat = 60.217165
+Hovi.lon = 24.394562
+Hovi.elevation = 80
+
 
 class SatelliteHandler:
     def __init__(self, ax, config, obs=None):
@@ -22,9 +21,10 @@ class SatelliteHandler:
         self.names = config["satellite"]["names"]
         self.filenames = config["satellite"]["files"]
         self.color = config["satellite"]["color"]
+        self.show_label = config["satellite"]["show_name"]
         self.ax = ax
         if obs is None:
-            obs = get_Hovi()
+            obs = Hovi
         self.observer = obs
         self.update()
         
@@ -46,24 +46,32 @@ class SatelliteHandler:
                     if name in self.names or not self.names:
                         sat = ephem.readtle(name, line1, line2)
                         point = self.ax.plot([],[], "o")[0]
+                        label = self.ax.text(0.0, 1000.0, "  " + name)
                         point.set_color(self.color)
-                        output[name] = (sat, point)
+                        label.set_color(self.color)
+                        label.set_fontsize("x-small")
+                        output[name] = (sat, point, label)
         self.satellites = output
         if self.DEBUG >= 2:
             sats = ", ".join(sorted(self.satellites.keys()))
             print("SatelliteHandler: following these satellites: {}".format(sats))
     
     def draw(self):
+        """Draw the satellites onto the Axes."""
         if self.DEBUG >= 3:
             print("SatelliteHandler: Drawing satellites.")
         self.observer.date = datetime.datetime.now()
-        for sat, point in self.satellites.values():
+        for sat, point, label in self.satellites.values():
             sat.compute(self.observer)
             alt = float(sat.alt)
             az = float(sat.az)
             if alt > 0.0:
                 point.set_data([az], [90 - alt*180/pi])
+                if self.show_label:
+                    label.set_position([az, 90 - alt*180/pi])
             else:
                 point.set_data([], [])
 
+    def draw_traces(self):
+        pass
 
