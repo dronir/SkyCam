45d14ccf1605a38b624884f7f5ca7f393bb4b73d
Olli Wilkman <olli.wilkman@iki.fi>
2016-08-15 11:24:38 +0300

    Add max_distance and show_eclipsed

--- src/satellite.py
+++ src/satellite.py
@@ -12,10 +12,6 @@ Hovi.lon = "24.394562"
 Hovi.elevation = 95
 Hovi.date = datetime.datetime.utcnow()
 
-
-
-print(Hovi)
-
 class SatelliteHandler:
     def __init__(self, ax, config):
         self.DEBUG = config["main"]["debug_level"]
@@ -31,6 +27,8 @@ class SatelliteHandler:
         self.trace_backward = config["satellite"]["trace_backward"]
         self.trace_show_time = config["satellite"]["trace_show_time"]
         self.min_altitude = config["satellite"]["min_altitude"] * pi/180
+        self.show_eclipsed = config["satellite"]["show_eclipsed"]
+        self.max_range = config["satellite"]["max_range"] * 1000.0
         self.ax = ax
         self.observer = Hovi
         self.update()
@@ -39,8 +37,7 @@ class SatelliteHandler:
         """Loads satellite orbit details from file."""
         if self.DEBUG >= 2:
             print("SatelliteHandler: Updating satellite list from files: {}".format(
-              ", ".join(self.filenames)
-            ))
+              ", ".join(self.filenames)))
         output = {}
         for filename in self.filenames:
             with open(filename) as f:
@@ -54,7 +51,8 @@ class SatelliteHandler:
                         sat = ephem.readtle(name, line1, line2)
                         point = self.ax.plot([],[], "o")[0]
                         trace = self.ax.plot([],[], "-")[0]
-                        label = self.ax.text(0.0, 1000.0, "  " + name)
+                        label = self.ax.text(0.0, 0.0, "  " + name)
+                        label.set_visible(False)
                         point.set_color(self.color)
                         point.set_markersize(8)
                         label.set_color(self.color)
@@ -75,14 +73,25 @@ class SatelliteHandler:
             sat.compute(self.observer)
             alt = float(sat.alt)
             az = float(sat.az)
-            if alt > self.min_altitude:
+            show = self.show_eclipsed or not sat.eclipsed
+            if show and alt > self.min_altitude and sat.range < self.max_range:
                 point.set_data([az], [90 - alt*180/pi])
                 if self.show_label:
+                    label.set_visible(True)
                     label.set_position([az, 90 - alt*180/pi])
             else:
                 point.set_data([], [])
+                continue
+            if sat.eclipsed:
+                point.set_color = "gray"
+                label.set_color = "gray"
+            else:
+                point.set_color = self.color
+                label.set_color = self.color
+            
 
     def draw_traces(self):
+        """Compute and draw the satellite traces."""
         if self.DEBUG >= 3:
             print("SatelliteHandler: Drawing satellite traces.")
         now = datetime.datetime.utcnow()
@@ -90,28 +99,31 @@ class SatelliteHandler:
         b = self.trace_forward + 1
         dt = self.trace_interval
         times = [now + datetime.timedelta(seconds=i*dt) for i in range(a, b)]
+        sat_idx = -a-1 # index corresponding to actual satellite location below
         for sat, point, label, trace in self.satellites.values():
             X = []
             Y = []
-            hidden = False
+            draw_label_later = False
             for i, time in enumerate(times):
                 self.observer.date = time
                 sat.compute(self.observer)
                 alt = float(sat.alt)
                 az = float(sat.az)
-                if alt > self.min_altitude:
+                show_eclipse = self.show_eclipsed or not sat.eclipsed
+                visible = (alt > self.min_altitude and sat.range < self.max_range 
+                           and show_eclipse)
+                if visible:
                     X.append(az)
                     Y.append(90 - alt*180/pi)
-                # Draw the label next to the trace if the satellite is below horizon
-                if i == -a-1 and alt < self.min_altitude:
-                    hidden = True
-                if (hidden and self.show_label and alt > self.min_altitude):
+                # Is the satellite itself below minimum altitude
+                if i == sat_idx and not visible:
+                    draw_label_later = True
+                # Draw the label at first visible point
+                if (draw_label_later and visible and self.show_label):
+                    label.set_visible(True)
                     label.set_position([az, 90 - alt*180/pi])
-                if i > -a-1 and alt > self.min_altitude:
-                    hidden = False
-                if hidden and i == len(times) - 1:
-                    X = []
-                    Y = []
-                    label.set_position([0.0, 1000.0])
+                    draw_label_later = False
+            if draw_label_later:
+                label.set_visible(False)
             trace.set_data(X,Y)
                 
